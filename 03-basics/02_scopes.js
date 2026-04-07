0bf11aa4d7cc2fba7a74f2242df77ca6b6b72110
HemantDohare <hemantdohare30@gamil.com>
2026-04-07 19:14:59 +0530

    scope

--- 03-basics/02_scopes.js
+++ 03-basics/02_scopes.js
@@ -36,3 +36,18 @@ if(true) {
 
 }
 //console.log(username);
+
+//++++++++++++++ intresting +++++++++++++++
+
+
+console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+
+console.log(addTwo(5));
+const addTwo = function(num){
+    return num + 2 
+}
+
