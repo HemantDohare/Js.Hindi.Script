7c720562e75ceb3b05ad0f39c6803c4776f9da83
HemantDohare <hemantdohare30@gamil.com>
2026-04-09 14:23:59 +0530

    controldone

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,60 @@
+// switch (key) {
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//          console.log("January");
+//          break;
+//     case 2:
+//          console.log("Feb");
+//          break;
+//     case 3:
+//          console.log("March");
+//          break;
+//     case 4:
+//          console.log("April");
+//          break;
+//     case 5:
+//          console.log("May");
+//          break;
+
+//     default:
+//         console.log(" deafault case match");
+//         break;     
+         
+// }
+
+
+
+const month = "March"
+
+switch (month) {
+    case "January":
+         console.log("January");
+         break;
+    case "Feb":
+         console.log("Feb");
+         break;
+    case "March":
+         console.log("March");
+         break;
+    case "April":
+         console.log("April");
+         break;
+    case "May":
+         console.log("May");
+         break;
+
+    default:
+        console.log(" deafault case match");
+        break;     
+         
+}
